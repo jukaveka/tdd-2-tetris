ea5ce22f108f22ae33d2830710c273af458a25d7
jukaveka <juhokavekari@hotmail.com>
2026-04-15 20:04:44 +0300

    Level 3 - Introduce orientations to Tetromino, start testing from T-shape

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -4,15 +4,28 @@ const T_SHAPE_ROWS = [`.T.`, `TTT`, `...`];
 const I_SHAPE_ROWS = [`.....`, `.....`, `IIII.`, `.....`, `.....`]
 
 export class Tetromino {
-  constructor(rows) {
-    this.shape = RotatingShape.fromArray(rows);
+  constructor(currentOrientation, orientations) {
+    this.currentOrientation = currentOrientation
+    this.orientations = orientations
   }
 
-  static T_SHAPE = new Tetromino(T_SHAPE_ROWS);
-  static I_SHAPE = new Tetromino(I_SHAPE_ROWS);
+  static T_SHAPE = Tetromino.create(T_SHAPE_ROWS, 4, 0);
+  // static I_SHAPE = new Tetromino(I_SHAPE_ROWS);
+
+  static create(rows, options, current) {
+    const shape = RotatingShape.fromArray(rows)
+    const orientations = [
+      shape,
+      shape.rotateRight(),
+      shape.rotateRight().rotateRight(),
+      shape.rotateLeft()
+    ].slice(0, options)
+    console.log(orientations)
+    return new Tetromino(current, orientations)
+  }
 
   toString() {
-    return this.shape.toString();
+    return this.orientations[this.currentOrientation].toString();
   }
 
   rotateRight() {

3ebf0475de0b302bf6f9f3ab5b0b043744d8bdee
jukaveka <juhokavekari@hotmail.com>
2026-04-17 20:52:51 +0300

    Level 5 - Create test for falling block moving to left

--- test/MovingTetrominoes.test.mjs
+++ test/MovingTetrominoes.test.mjs
@@ -3,19 +3,33 @@ import { expect } from "chai";
 import { Board } from "../src/Board.mjs";
 import { Tetromino } from "../src/Tetromino.mjs";
 
-describe("Falling tetrominoes", () => {
+describe("Moving tetrominoes", () => {
   let board;
   beforeEach(() => {
     board = new Board(10, 6);
   });
 
-  test("can be moved right", () => {
+  test("to right succeeds", () => {
     board.drop(Tetromino.T_SHAPE);
     board.moveBlockRight();
 
     expect(board.toString()).to.equalShape(
       `.....T....
        ....TTT...
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+  });
+
+  test("to left succeeds", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.moveBlockLeft();
+
+    expect(board.toString()).to.equalShape(
+      `...T......
+       ..TTT.....
        ..........
        ..........
        ..........

b86570746a84727bff8d17c82d7943444b51d449
jukaveka <juhokavekari@hotmail.com>
2026-05-02 18:07:32 +0300

    Add character property to tetromino

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -5,26 +5,27 @@ const I_SHAPE_ROWS = [`.....`, `.....`, `IIII.`, `.....`, `.....`];
 const O_SHAPE_ROWS = [`.OO`, `.OO`, `...`];
 
 export class Tetromino {
-  constructor(currentOrientation, orientations) {
+  constructor(currentOrientation, orientations, character) {
     this.currentOrientation = (currentOrientation + orientations.length) % orientations.length;
     this.orientations = orientations;
+    this.character = character;
   }
 
-  static T_SHAPE = Tetromino.create(T_SHAPE_ROWS, 4, 0);
-  static I_SHAPE = Tetromino.create(I_SHAPE_ROWS, 2, 0);
-  static O_SHAPE = Tetromino.create(O_SHAPE_ROWS, 1, 0);
+  static T_SHAPE = Tetromino.create(T_SHAPE_ROWS, 4, 0, "T");
+  static I_SHAPE = Tetromino.create(I_SHAPE_ROWS, 2, 0, "I");
+  static O_SHAPE = Tetromino.create(O_SHAPE_ROWS, 1, 0, "O");
   static SINGLE_BLOCK(character) {
     const row = [character];
-    return Tetromino.create(row, 1, 0);
+    return Tetromino.create(row, 1, 0, character);
   }
 
-  static create(rows, orientationCount, current) {
+  static create(rows, orientationCount, current, character) {
     const shape = RotatingShape.fromArray(rows);
     const orientations = [shape, shape.rotateRight(), shape.rotateRight().rotateRight(), shape.rotateLeft()].slice(
       0,
       orientationCount
     );
-    return new Tetromino(current, orientations);
+    return new Tetromino(current, orientations, character);
   }
 
   current() {

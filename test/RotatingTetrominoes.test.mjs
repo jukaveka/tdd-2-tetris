22bdd371dd2565f9d088f4ee44570ee3dd20a78f
jukaveka <juhokavekari@hotmail.com>
2026-05-04 20:56:55 +0300

    Level 7 - Test right rotation of L-shape

--- test/RotatingTetrominoes.test.mjs
+++ test/RotatingTetrominoes.test.mjs
@@ -132,4 +132,12 @@ describe("The L-shape", () => {
     );
   });
 
+  test("can be rotated right/clockwise", () => {
+    expect(shape.rotateRight().toString()).to.equalShape(
+      `LL..
+       .L..
+       .L..
+       ....`
+    );
+  });
 })

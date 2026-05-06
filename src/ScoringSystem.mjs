ef5930200e506c25b96f393232a60c40356126f8
jukaveka <juhokavekari@hotmail.com>
2026-05-06 20:48:07 +0300

    Level 9 - Add level property and getters to scoring system

--- src/ScoringSystem.mjs
+++ src/ScoringSystem.mjs
@@ -1,11 +1,17 @@
 export class ScoringSystem {
   #points = 0;
+  #level = 0;
 
   constructor() {
     this.#points;
+    this.#level;
   };
 
   points() {
     return this.#points;
+  }
+
+  level() {
+    return this.#level;
   }
 }
